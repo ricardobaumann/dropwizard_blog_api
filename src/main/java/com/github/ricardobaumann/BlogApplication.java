f6341ec9703fa473936114f96360274df57e9d70
ricardobaumann <ricardo.luis.baumann@gmail.com>
2016-12-15 13:50:09 +0100

    wip

--- src/main/java/com/github/ricardobaumann/BlogApplication.java
+++ src/main/java/com/github/ricardobaumann/BlogApplication.java
@@ -1,5 +1,12 @@
 package com.github.ricardobaumann;
 
+import com.google.inject.Inject;
+import io.dropwizard.client.HttpClientBuilder;
+import io.dropwizard.lifecycle.Managed;
+import org.apache.http.client.entity.EntityBuilder;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
 
 import com.github.ricardobaumann.db.User;
@@ -12,12 +19,16 @@ import io.dropwizard.migrations.MigrationsBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
+import java.net.URI;
+
 public class BlogApplication extends Application<BlogConfiguration> {
 
     public static void main(final String[] args) throws Exception {
 
         new BlogApplication().run(args);
     }
+
+    //private static final MyManaged MY_MANAGED = new MyManaged(environment);
 
     @Override
     public String getName() {
@@ -38,6 +49,8 @@ public class BlogApplication extends Application<BlogConfiguration> {
                 return configuration.getDataSourceFactory();
             }
         });
+
+
     }
 
     @SuppressWarnings({ "unchecked", "rawtypes" })
@@ -45,6 +58,60 @@ public class BlogApplication extends Application<BlogConfiguration> {
     public void run(final BlogConfiguration configuration, final Environment environment) {
       environment.jersey().register(RolesAllowedDynamicFeature.class);
       environment.jersey().register(new AuthValueFactoryProvider.Binder(User.class));
+
+        environment.lifecycle().manage(new MyManaged(environment));
+
+    }
+
+    public static class MyManaged implements Managed {
+
+        private static CloseableHttpClient client;
+
+        @Inject
+        public MyManaged(Environment environment) {
+            if (client==null) {
+                client = new HttpClientBuilder(environment).build("eureka");
+            }
+
+        }
+
+        @Override
+        public void start() throws Exception {
+            System.out.println("Yayyyyy, started");
+            HttpPost post = new HttpPost();
+            String text = "{\n" +
+                    "    \"instance\": {\n" +
+                    "        \"hostName\": \"WKS-SOF-L011\",\n" +
+                    "        \"app\": \"dwblog_app\",\n" +
+                    "        \"vipAddress\": \"localhost\",\n" +
+                    "        \"secureVipAddress\": \"localhost\"\n" +
+                    "        \"ipAddr\": \"localhost\",\n" +
+                    "        \"status\": \"STARTING\",\n" +
+                    "        \"port\": {\"$\": \"8080\", \"@enabled\": \"true\"},\n" +
+                    "        \"securePort\": {\"$\": \"8443\", \"@enabled\": \"false\"},\n" +
+                   // "        \"healthCheckUrl\": \"http://WKS-SOF-L011:8080/healthcheck\",\n" +
+                    //"        \"statusPageUrl\": \"http://WKS-SOF-L011:8080/status\",\n" +
+                    //"        \"homePageUrl\": \"http://WKS-SOF-L011:8080\",\n" +
+                    "        \"dataCenterInfo\": {\n" +
+                    "            \"@class\": \"com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo\", \n" +
+                    "            \"name\": \"MyOwn\"\n" +
+                    "        },\n" +
+                    "    }\n" +
+                    "}";
+            post.setEntity(EntityBuilder.create().setText(text).build());
+
+            post.setHeader("Content-Type","application/json");
+
+            post.setURI(new URI("http://localhost:1111/eureka/apps/dwblog_app"));
+
+            CloseableHttpResponse res = client.execute(post);
+            res.getEntity().writeTo(System.out);
+        }
+
+        @Override
+        public void stop() throws Exception {
+            System.out.println("Yayyyyy, stop");
+        }
     }
 
 }
